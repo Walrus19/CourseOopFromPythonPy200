85938ccbe054495cabaa4996dc6c650f9f9dc36d
Walrus19 <dick19@yandex.ru>
2025-01-31 08:28:16 +0300

    work2

--- Занятие 1. Основы ООП/2. Лабораторные задания/task1_CustomClass/Chair.py
+++ Занятие 1. Основы ООП/2. Лабораторные задания/task1_CustomClass/Chair.py
@@ -1,6 +1,6 @@
 class Chair:
     color_pallet = ['red', 'black']
-    def __init__(self, color, height):
+    def __init__(self, color: str, height: (int,float)):
         """
 
         :param color:
@@ -19,7 +19,10 @@ class Chair:
         self.height = height
 
     def change_color(self, new_color):
-        if new_color in color_pallet:
+        if new_color in self.color_pallet:
             self.color = new_color
             print ("Удачное изменение цвета")
         return self.color
+
+ch1 = Chair('red', 100)
+ch1.change_color('black')
